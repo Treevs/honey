e7bace9876817ed9f562d77fef6ea47325bd82b4
Treevs <tvr.jomar@gmail.com>
2017-03-29 00:39:39 -0500

    Dynamic hexagon background now works on Meteor page

--- honey/client/main.js
+++ honey/client/main.js
@@ -19,3 +19,55 @@ if (Meteor.isCordova) {
 } else {
   angular.element(document).ready(onReady);
 }
+
+var width = window.innerWidth
+                    || document.documentElement.clientWidth
+                    || document.body.clientWidth;
+
+        var height = window.innerHeight
+                    || document.documentElement.clientHeight
+                    || document.body.clientHeight;
+        
+        var rowWidth = width/104
+        var numOfRows = height /52;
+        var hexagons = document.getElementById("hexagons");
+        for( var i=0; i<numOfRows; i++) {
+            var row = document.createElement("div")
+            if(i%2 == 0)
+                row.className = "hex-row even";
+            else
+                row.className = "hex-row";
+
+            for(var j=0; j<rowWidth; j++) {
+                var hexagon = createHexagon();
+                row.appendChild(hexagon);
+            }
+            hexagons.appendChild(row);
+        }
+
+        function createHexagon() {
+
+            var colors = ["D9B611", "F3C13A", "F7CA18", "E2B13C", "A17917", "F5D76E", "F4D03F", "FFA400", "E08A1E" , 
+                            "FFB61E", "FAA945", "FFA631", "FFB94E", "E29C45", "CA6924", "F5AB35"]; //Not pictured: #F9690E
+            var randomColor = "#" + colors[Math.floor(Math.random()*colors.length)]; 
+            var hexagon = document.createElement("div");
+            hexagon.className = "hexagon";
+            var top = document.createElement("div");
+            top.className = "hex-top";
+            top.style.borderBottomColor = randomColor;
+            
+            var middle = document.createElement("div");
+            middle.className = "hex-middle";
+            middle.style.background = randomColor;
+            
+            var bottom = document.createElement("div");
+            bottom.className = "hex-bottom";
+            bottom.style.borderTopColor = randomColor;
+
+            hexagon.appendChild(top);
+            hexagon.appendChild(middle);
+            hexagon.appendChild(bottom);
+
+            return hexagon;
+
+        }
